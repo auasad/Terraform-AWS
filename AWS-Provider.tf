31e382d7d6bdbe2a4068f898f95ae03d10691b30
auasad <36460334+auasad@users.noreply.github.com>
2018-06-09 14:56:32 +0500

    removed access and secret key

--- AWS-Provider.tf
+++ AWS-Provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = "AKIAIIJPYU3R7VP7W6GQ"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "${var.aws_region}"
 }
